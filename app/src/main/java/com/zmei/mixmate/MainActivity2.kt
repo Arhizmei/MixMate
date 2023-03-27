346f04bb2fe5dcd8261810cbd4a3aec55a347739
Arhizmei <arhizmei@gmail.com>
2023-03-27 22:56:29 +0300

    1.3

--- app/src/main/java/com/zmei/mixmate/MainActivity2.kt
+++ app/src/main/java/com/zmei/mixmate/MainActivity2.kt
@@ -2,6 +2,10 @@ package com.zmei.mixmate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.zmei.mixmate.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
@@ -10,5 +14,19 @@ class MainActivity2 : AppCompatActivity() {
         bind= ActivityMain2Binding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(bind.root)
+
+        val spinner: Spinner = bind.spinner1
+        val spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.cement_grade, android.R.layout.simple_spinner_item)
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = spinnerAdapter
+
+
+    }
+
+
+
+
+    fun exit_button(view: View){
+        finishAffinity()
     }
 }
