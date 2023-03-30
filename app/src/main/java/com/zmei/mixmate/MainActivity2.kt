44ee01dfd51f2f9c1728f865362ae169c5c9965a
Arhizmei <arhizmei@gmail.com>
2023-03-30 13:18:03 +0300

    2.0

--- app/src/main/java/com/zmei/mixmate/MainActivity2.kt
+++ app/src/main/java/com/zmei/mixmate/MainActivity2.kt
@@ -10,18 +10,53 @@ import com.zmei.mixmate.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
     lateinit var bind : ActivityMain2Binding
+    lateinit var  spinner: Spinner
     override fun onCreate(savedInstanceState: Bundle?) {
         bind= ActivityMain2Binding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(bind.root)
 
-        val spinner: Spinner = bind.spinner1
+
+        spinner =  bind.spinner1
         val spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.cement_grade, android.R.layout.simple_spinner_item)
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = spinnerAdapter
-
         val selectedItem = spinner.selectedItem.toString()
 
+        }
+
+        fun onClickOk (view: View){
+            var cube = 0
+            var sand = 0
+            var gravel = 0
+            var cement = 0
+
+            spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                    if (position == 0) {
+
+                    bind.tvMessage.visibility = View.VISIBLE
+                    bind.tvMessage.text = "Выберите марку цемента!"
+
+
+                    } else if (position == 1) {
+                    var cube1 = bind.edCube.text.toString()
+                        cube = cube1.toInt()
+                    sand = cube *  870
+                        bind.tvSand.text = "$sand"
+                    gravel = cube *  1080
+                        bind.tvGravel.text = "$gravel"
+                    cement = cube * 214
+                        bind.tvCement.text = "$cement"
+
+                    }
+
+                }
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    bind.tvMessage.visibility = View.VISIBLE
+                    bind.tvMessage.text = "Выберите марку цемента!"
+                }
+        }
     }
 
 
